00539617013de6c1913dbc6376083f704382deb4
Scott <admin@gearproject.co>
2022-02-19 14:02:24 -0700

    pooling

--- db/getOperatorByUserId.js
+++ db/getOperatorByUserId.js
@@ -1,20 +1,24 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    ssl: process.env.NODE_ENV !== 'development' ? { rejectUnauthorized: false, ca: Buffer.from(process.env.PG_CA, 'base64').toString('ascii') } : null,
+    max: 20,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0
+});
 
 export const getOperatorByUserId = async (userId, getAll) => {
     let dbResponse = [];
 
     try {
-        const client = new Client({ ssl: process.env.NODE_ENV !== 'development' ? { rejectUnauthorized: false, ca: Buffer.from(process.env.PG_CA, 'base64').toString('ascii') } : null });
-        await client.connect();
         const text = 'SELECT * FROM operators WHERE user_id = $1 ORDER BY operator_last_edit_date DESC';
         const values = [userId];
-        const res = await client.query(text, values);
+        const res = await pool.query(text, values);
         if (getAll) {
             dbResponse = res?.rows;
         } else {
             dbResponse = res?.rows?.[0];
         }
-        await client.end();
     } catch (error) {
         console.log('error getting users from db: ', error);
     }
