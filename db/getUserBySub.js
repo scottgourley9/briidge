847f99eff452557dcf1539af4a0a962b2e30e5e0
Scott <admin@gearproject.co>
2022-02-19 13:54:00 -0700

    testing

--- db/getUserBySub.js
+++ db/getUserBySub.js
@@ -2,7 +2,9 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     ssl: process.env.NODE_ENV !== 'development' ? { rejectUnauthorized: false, ca: Buffer.from(process.env.PG_CA, 'base64').toString('ascii') } : null,
-    max: 20
+    max: 20,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0
 });
 
 export const getUserBySub = async sub => {
