51973fef8947deefcbeb96bf5e3d3f30ecb76900
Scott <admin@gearproject.co>
2022-02-20 13:30:20 -0700

    update max

--- db/initialize.js
+++ db/initialize.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     ssl: process.env.NODE_ENV !== 'development' ? { rejectUnauthorized: false, ca: Buffer.from(process.env.PG_CA, 'base64').toString('ascii') } : null,
-    max: 20,
+    max: 90,
     connectionTimeoutMillis: 5000,
     idleTimeoutMillis: 30000
 });
